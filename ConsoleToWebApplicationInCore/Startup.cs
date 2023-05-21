84a0aa6a4feff74bf84c30c8ae8018a5c7ca5b37
ankitt0786 <ankitsingh7295@gmail.com>
2023-05-21 12:04:14 +0530

    Add project files.

---
+++ ConsoleToWebApplicationInCore/Startup.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleToWebApplicationInCore
+{
+    public class Startup
+    {
+        //this method is use to setup our dependencies
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //use to set up the dependencies like mvc or api or razor pages
+
+            services.AddControllersWithViews();
+        }
+
+        //this method control the request pipeline with the help of middlewares
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //set up your development environment here
+            //add all the middleware define here
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                //this the default 
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello to your first application");
+                //});
+
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/ankit", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Ankit.");
+                });
+            });
+        }
+    }
+}
