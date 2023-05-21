c884a2730f7b20a708718d183073711c443d8ebd
ankitt0786 <ankitsingh7295@gmail.com>
2023-05-21 13:46:41 +0530

    Create a Controller constructor and use ReposMethod

--- ConsoleToWebApplicationInCore/Controllers/HomeController.cs
+++ ConsoleToWebApplicationInCore/Controllers/HomeController.cs
@@ -1,16 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ConsoleToWebApplicationInCore.Models;
+using ConsoleToWebApplicationInCore.Repository;
 
 namespace ConsoleToWebApplicationInCore.Controllers
 {
     public class HomeController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-        public string Index()
+        private readonly MetodRepos _db = null;
+        public HomeController()
         {
-            return "Ankit";
+            _db = new MetodRepos();
+        }
+
+        public List<BookModel> AllBooks()
+        {
+            return _db.GetBookList();
+        }
+
+        public BookModel Singlebook(int id)
+        {
+            return _db.GetBook(id);
+        }
+
+        public List<BookModel> SearchBook(string p1, string p2)
+        {
+            return _db.GetBooksSearch(p1,p2);
         }
     }
 }
---
+++ ConsoleToWebApplicationInCore/Repository/MetodRepos.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleToWebApplicationInCore.Models;
+
+namespace ConsoleToWebApplicationInCore.Repository
+{
+    public class MetodRepos
+    {
+        public List<BookModel> GetBookList()
+        { 
+            return books(); 
+        }
+        public BookModel GetBook(int id) 
+        {  
+            return books().Where(x=>x.Id == id).FirstOrDefault(); 
+        }
+
+        public List<BookModel> GetBooksSearch(string title, string name)
+        {
+            return books().Where(x=>x.Title.Contains(title) || x.Author==name).ToList();
+        }
+
+
+        private List<BookModel> books()
+        {
+            List<BookModel> bookModels = new List<BookModel>()
+            {
+                new BookModel()
+                {
+                Id=1,
+                Title="firstbook",
+                Description="This is a dummy book",
+                Author="Ankit",
+                AuthorUrl="Ankit.com"
+                },
+                new BookModel()
+                {
+                    Id = 2,
+                    Title = "second book",
+                    Description = "This is a dummy book",
+                    Author = "Vinay",
+                    AuthorUrl = "vinay.com"
+                }
+            };
+
+            return bookModels;
+        }
+    }
+}
