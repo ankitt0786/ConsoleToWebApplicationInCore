d630a9292f3d21529c6d8b322016fa3499bdeba8
ankitt0786 <ankitsingh7295@gmail.com>
2023-05-21 13:55:20 +0530

    Add View

---
+++ ConsoleToWebApplicationInCore/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleToWebApplicationInCore.Models;
+using ConsoleToWebApplicationInCore.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebApplicationInCore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly MetodRepos _db = null;
+        public BookController()
+        {
+            _db = new MetodRepos();
+        }
+
+        public List<BookModel> AllBooks()
+        {
+            return _db.GetBookList();
+        }
+
+        public BookModel Singlebook(int id)
+        {
+            return _db.GetBook(id);
+        }
+
+        public List<BookModel> SearchBook(string p1, string p2)
+        {
+            return _db.GetBooksSearch(p1, p2);
+        }
+    }
+}
--- ConsoleToWebApplicationInCore/Controllers/HomeController.cs
+++ ConsoleToWebApplicationInCore/Controllers/HomeController.cs
@@ -6,25 +6,9 @@ namespace ConsoleToWebApplicationInCore.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly MetodRepos _db = null;
-        public HomeController()
+        public IActionResult Index()
         {
-            _db = new MetodRepos();
-        }
-
-        public List<BookModel> AllBooks()
-        {
-            return _db.GetBookList();
-        }
-
-        public BookModel Singlebook(int id)
-        {
-            return _db.GetBook(id);
-        }
-
-        public List<BookModel> SearchBook(string p1, string p2)
-        {
-            return _db.GetBooksSearch(p1,p2);
+            return View();
         }
     }
 }
